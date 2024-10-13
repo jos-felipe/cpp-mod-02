b680adbe231d09bd7d54152f2fb2757b9a29d8ae
josfelip@debian <josfelip@debian>
2024-10-13 20:25:34 -0300

    first commit

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,21 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/01 22:26:19 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/10/01 11:18:42 by josfelip         ###   ########.fr       */
+/*   Created: 2024/08/22 10:21:59 by tmina-ni          #+#    #+#             */
+/*   Updated: 2024/08/22 13:47:07 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include "Fixed.hpp"
 
-int	main(void) {
-	Zombie *heap_zombie = newZombie("HeapHopper");
-	heap_zombie->announce();
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
 
-	randomChump("StackWalker");
+	c = b;
 
-	delete heap_zombie;
-	return (0);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
 }
